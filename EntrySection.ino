6c9e9cc760787e1d62b789284f2ea942a6b1aff2
Thomas Frenz <tfrenz67@gmail.com>
2018-11-14 21:13:54 +0100

    clean up tab

--- EntrySection.ino
+++ EntrySection.ino
@@ -1,13 +1,16 @@
-void startEntrySequence() { // print Startbildschirm
+// print start screen
+// wait for pushing "B" to start game
+
+void startEntrySequence() {
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(0, 80);
   M5.Lcd.setTextColor(M5.Lcd.color565(255, 131, 250));
   M5.Lcd.setTextSize(3);
+  M5.Lcd.setCursor(0, 80);
   M5.Lcd.println(" RJPlog's ");
   M5.Lcd.println(" Crazy Asteroids");
-  M5.Lcd.setCursor(100, 210);
   M5.Lcd.setTextColor(M5.Lcd.color565(139, 0, 139));
   M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(100, 210);
   M5.Lcd.println("  press B ");
   while (1) {
     // wait for push button
